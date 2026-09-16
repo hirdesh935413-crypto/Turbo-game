aace74261f9e587789ff7ecf516d643a1aa0f660
hirdesh935413-crypto <hirdesh935413@gmail.com>
2026-09-16 18:29:09 +0530

    Create GameProfile.java

---
+++ app/src/main/java/com/gameturbopro/GameProfile.java
@@ -0,0 +1,15 @@
+package com.gameturbopro;
+
+public class GameProfile {
+
+    public String packageName = "";
+    public String dpi = "";
+    public String brightness = "";
+    public String timeout = "";
+    public String refreshRate = "";
+    public String sensitivity = "";
+    public String graphics = "";
+
+    public boolean gamingMode = true;
+    public boolean dnd = false;
+}
